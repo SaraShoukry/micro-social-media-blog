f66aac18883de43c12acc340900b28c3bcec2144
SaraShokry <s.yahia93@gmail.com>
2019-08-31 20:21:00 +0200

    create migrations

---
+++ database/migrations/2019_08_31_180000_create_tweets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->text('tweet')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tweets', function (Blueprint $table) {
+            $table->dropForeign(['user_id']); // drop the foreign key.
+            $table->dropColumn('user_id'); // drop the column
+        });
+        Schema::dropIfExists('tweets');
+    }
+}
