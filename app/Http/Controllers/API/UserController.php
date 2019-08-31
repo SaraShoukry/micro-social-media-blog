5eae9895dab255d01cdb2d6f5aaef5f7f80eec4a
SaraShokry <s.yahia93@gmail.com>
2019-08-31 12:30:09 +0200

    logout issue

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Requests\Register;
+use App\Traits\UploadTrait;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class UserController extends Controller
+{
+
+    public $successStatus = 200;
+
+    /**
+     * login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login()
+    {
+        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('MyApp')->accessToken;
+            $user->setAPIToken($success['token']);
+            return response()->json(['success' => $success], $this->successStatus);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
+        }
+    }
+
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+            'profile_picture' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 401);
+        }
+
+        $input = $request->all();
+        $user = new User();
+        $user = $user->register($input);
+        $success['name'] = $user->name;
+        $success['token'] = $user->api_token;
+        return response()->json(['success' => $success], $this->successStatus);
+    }
+
+    public function logout(Request $request)
+    {
+        if (Auth::check()) {
+            $request->user()->token()->delete();
+        }
+        return response()->json(['message' => 'User logged out .'], 200);
+    }
+
+    /**
+     * details api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function details()
+    {
+        $user = Auth::user();
+        return response()->json(['success' => $user], $this->successStatus);
+    }
+}
--- app/User.php
+++ app/User.php
@@ -2,13 +2,17 @@
 
 namespace App;
 
+use App\Traits\UploadTrait;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
+    use UploadTrait;
 
     /**
      * The attributes that are mass assignable.
@@ -16,7 +20,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'profile_image'
     ];
 
     /**
@@ -36,4 +40,33 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getImageAttribute()
+    {
+        return $this->profile_image;
+    }
+
+    public function register($data){
+
+        $profileImage = $data['profile_picture'];
+        $profileImageSaveAsName = time() . "-profile." . $profileImage->getClientOriginalExtension();
+        $upload_path = 'profile_images/';
+        $profile_image_url = $upload_path . $profileImageSaveAsName;
+//        $profileImage->move($upload_path, $profileImageSaveAsName);
+        $this->uploadOne($profileImage, $upload_path, 'public', $profileImageSaveAsName);
+
+
+        $input['password'] = bcrypt($data['password']);
+        $user = User::create($data);
+        $success['token'] = $user->createToken('MyApp')->accessToken;
+        $user->api_token = $success['token'];
+        $user->profile_picture = $profile_image_url;
+        $user->save();
+        return $user;
+    }
+
+    public function setAPIToken($api_token){
+        $this->api_token = $api_token;
+        return $this->save();
+    }
 }
